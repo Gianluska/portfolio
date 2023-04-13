ee413572efef97dae8a7d4a4704e2104c46fdcaa
Gianlucca Claudino <gianluccaclaudino@gmail.com>
2023-04-12 23:30:19 -0300

    fix: Removing unused code

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -7,13 +7,16 @@ import * as THREE from "three";
 import { Physics, useSphere } from "@react-three/cannon";
 import {
   Sky,
+  useTexture
 } from "@react-three/drei";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
+
+// import Cross from '../../../public/cross.jpg'
 
 const rfs = THREE.MathUtils.randFloatSpread;
 const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
 const baubleMaterial = new THREE.MeshStandardMaterial({
-  color: "black",
+  color: "#000",
   roughness: 100,
   envMapIntensity: 0,
 });
@@ -24,16 +27,15 @@ export const Intro = () => (
     dpr={[1, 2]}
     camera={{ position: [0, 0, 20], fov: 35, near: 1, far: 40 }}
   >
-    <ambientLight intensity={0.25} />
     <spotLight
       intensity={1}
       angle={0.2}
       penumbra={1}
       position={[30, 30, 30]}
       castShadow
-      shadow-mapSize={[512, 512]}
+      shadow-mapSize={[128, 128]}
     />
-    <directionalLight intensity={5} position={[-10, -10, -10]} color="purple" />
+    <directionalLight intensity={2} position={[-10, -10, -10]} color="#8906bd" />
     <Physics gravity={[0, 2, 0]} iterations={10}>
       <Pointer />
       <Clump />
@@ -46,16 +48,15 @@ function Clump({
   vec = new THREE.Vector3(),
   ...props
 }) {
-  // const texture = useTexture("/cross.jpg");
   const [ref, api] = useSphere(() => ({
     args: [1],
-    mass: 1,
+    mass: 3,
     angularDamping: 0.1,
     linearDamping: 0.65,
     position: [rfs(20), rfs(20), rfs(20)],
   }));
   useFrame((state) => {
-    for (let i = 0; i < 40; i++) {
+    for (let i = 0; i < 35; i++) {
       // Get current whereabouts of the instanced sphere
       ref?.current?.getMatrixAt(i, mat);
       // Normalize the position and multiply by a negative force.
@@ -77,10 +78,9 @@ function Clump({
       ref={ref}
       castShadow
       receiveShadow
-      args={[null, null, 40]}
+      args={[null, null, 35]}
       geometry={sphereGeometry}
       material={baubleMaterial}
-      // material-map={texture}
     />
   );
 }
