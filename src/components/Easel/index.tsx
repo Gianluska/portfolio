5545f5ebc5ed04484b6227c72fbc155b2ea8cadd
Gianlucca Claudino <gianluccaclaudino@gmail.com>
2024-10-15 22:35:19 -0300

    Refactor Easel component to load multiple 3D models

--- src/components/Easel/index.tsx
+++ src/components/Easel/index.tsx
@@ -2,6 +2,19 @@ import { useGLTF } from "@react-three/drei";
 
 export function Easel() {
   const { scene } = useGLTF('/models/easel.glb');
+  const seila1 = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/anvil/model.gltf');
+  const seila2 = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/ruins/model.gltf');
+  const seila3 = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/desk/model.gltf');
+  const seila5 = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/giraffe/model.gltf');
+  const seila6 = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/korrigan-wolf/model.gltf');
+  const seila7 = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/ankou-with-cart/model.gltf');
+  const seila8 = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/korrigan-taning/model.gltf');
+  const seila9 = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/old-korrigan/model.gltf');
+  const seila10 = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/druid/model.gltf');
+  const seila11 = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/barn/model.gltf');
+  const seila12 = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/turntable/model.gltf');
+  const seila13 = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/table/model.gltf');
+  const seila15 = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/bunny/model.gltf');
 
   return (
     <group>
--- src/pages/Loading/index.tsx
+++ src/pages/Loading/index.tsx
@@ -3,11 +3,10 @@ import { useProgress } from "@react-three/drei";
 export function LoadingScreen() {
   const { progress } = useProgress();
 
-  console.log(progress);
-  
   return (
-    <div>
-      <h1>Loading...</h1>
+    <div className="bg-black fixed top-0 bottom-0 left-0 right-0 flex justify-center items-center">
+      <h1 className="text-white">Loading...</h1>
+      <progress value={progress} max={100} />
     </div>
   );
 }
