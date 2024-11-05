3f3c5b00363843e4543d902adcf9a8233de099c4
Gianlucca Claudino <gianluccaclaudino@gmail.com>
2024-11-05 02:18:42 -0300

    Enhance InteractiveCanvas with portal effects and ambient lighting

--- src/components/InteractiveCanvas/index.tsx
+++ src/components/InteractiveCanvas/index.tsx
@@ -1,8 +1,18 @@
+import { MeshPortalMaterial } from "@react-three/drei";
+import PortalParticles from '@components/Sphere/portalParticles';
+import MysticFogSphere from '@components/Sphere';
+
 export function InteractiveCanvas() {
   return (
     <mesh position={[-0.3, 3.8, 0.46]} rotation-x={-0.2}>
       <planeGeometry args={[3.3, 4.5]} />
-      <meshBasicMaterial color="pink" />
+      {/* Portal */}
+      <MeshPortalMaterial transparent={true} depthWrite={false} depthTest={false}>
+        <ambientLight intensity={0.5} />
+        <MysticFogSphere />
+      </MeshPortalMaterial>
+      {/* Partículas Envolventes */}
+      <PortalParticles />
     </mesh>
   );
 }
