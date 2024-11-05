e872ee5ff31b0fb9b341c17580bd18dbd479aa1b
Gianlucca Claudino <gianluccaclaudino@gmail.com>
2024-11-04 23:24:09 -0300

    Update InteractiveCanvas mesh position for improved alignment

--- src/components/InteractiveCanvas/index.tsx
+++ src/components/InteractiveCanvas/index.tsx
@@ -1,6 +1,6 @@
 export function InteractiveCanvas() {
   return (
-    <mesh position={[-0.3, 3.8, 0.56]} rotation-x={-0.2}>
+    <mesh position={[-0.3, 3.8, 0.46]} rotation-x={-0.2}>
       <planeGeometry args={[3.3, 4.5]} />
       <meshBasicMaterial color="pink" />
     </mesh>
